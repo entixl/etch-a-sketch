3b05d9ee1a4db6d5ad9d797ce5d2fa20418e748d
ential <xdnotavailable@gmail.com>
2025-10-05 04:10:57 +0530

    fuck total grid selection

--- script.js
+++ script.js
@@ -1,35 +1,6 @@
 const gridSection = document.querySelector(".grid-section");
-const totalGridButtonSection = document.querySelector(
-	".total-grid-button-section"
-);
-const totalGridButtonOptions = [];
-for (let i = 1; i < 5; i++) {
-	const intialTotalGrid = 4;
-	let totalGrid = intialTotalGrid ** i;
-	totalGridButtonOptions.push({
-		value: totalGrid,
-		text: `${totalGrid / 2}x${totalGrid / 2}`,
-	});
-}
-totalGridButtonOptions.forEach((totalButtonOption) => {
-	const totalGridButton = document.createElement("button");
-	totalGridButton.classList.add("total-grid-button");
-	totalGridButton.value = totalButtonOption.value;
-	totalGridButton.innerText = totalButtonOption.text;
-	totalGridButtonSection.append(totalGridButton);
-});
-const totalGridButtonList = document.querySelectorAll(".total-grid-button");
-totalGridButtonList.forEach((button) => {
-	button.addEventListener("click", () => {
-		const totalGrid = button.value;
-		createNewGrid(totalGrid);
-	});
-});
-function createNewGrids(totalGrid) {
-	gridSection.innerHTML = "";
-	for (let i = 0; i < totalGrid; i++) {
-		const gridButton = document.createElement("button");
-		gridButton.classList.add("grid-button");
-		gridSection.append(gridButton);
-	}
+for (let i = 0; i < 16 * 16; i++) {
+	const gridButton = document.createElement("button");
+	gridButton.classList.add("grid-button");
+	gridSection.append(gridButton);
 }
