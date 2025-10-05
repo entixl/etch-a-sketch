68ccf11acfc9f9b6a60035183d13eb21b118751c
ential <xdnotavailable@gmail.com>
2025-10-05 06:01:32 +0530

    add change color

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ for (let i = 0; i < 16 * 16; i++) {
 	gridButton.classList.add("grid-button");
 	gridSection.append(gridButton);
 }
+const gridButtonList = document.querySelectorAll(".grid-button");
+gridButtonList.forEach((gridButton) => {
+	gridButton.addEventListener("mouseenter", () => {
+		console.log(gridButton);
+		changeGridColor(gridButton);
+	});
+});
+function changeGridColor(button) {
+	button.classList.add("button-black-color");
+}
